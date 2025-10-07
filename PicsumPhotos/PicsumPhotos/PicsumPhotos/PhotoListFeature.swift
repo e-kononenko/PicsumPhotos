1c326111ee09d848ba73de97b99edf9304393e2c
e-kononenko <yevgeny.kononenko@gmail.com>
2025-10-07 18:30:33 +0200

    started with PhotosFeature

--- PicsumPhotos/PicsumPhotos/PicsumPhotos/PhotoListFeature.swift
+++ PicsumPhotos/PicsumPhotos/PicsumPhotos/PhotoListFeature.swift
@@ -4,4 +4,40 @@
 //
 //  Created by Evgenii Kononenko on 06.10.25.
 //
+import Foundation
+import ComposableArchitecture
 
+struct Photo: Identifiable, Equatable {
+    let id: String
+    let author: String
+    let url: URL?
+}
+
+
+struct PhotoListFeature {
+    @ObservableState
+    struct State: Equatable {
+        var photos: [Photo] = []
+        var isLoading: Bool = false
+        var errorText: String?
+    }
+
+    enum Action {
+        case didAppear
+        case fetchPhotosSuccess([Photo])
+        case fetchPhotosFailure(Error)
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .didAppear:
+                return .none
+            case .fetchPhotosSuccess(let photos):
+                return .none
+            case .fetchPhotosFailure(let error):
+                return .none
+            }
+        }
+    }
+}
